c90d2a50316d36705b397c929d429b4bf1c2b33f
Mateusz <mateusz.wolnica@gmail.com>
2024-04-18 15:09:46 +0200

    .

--- Transmiter/TransmiterForm.cs
+++ Transmiter/TransmiterForm.cs
@@ -1,3 +1,5 @@
+using Logic.Services;
+
 namespace Transmiter
 {
     public partial class TransmiterForm : Form
@@ -10,7 +12,27 @@ namespace Transmiter
         private void TransmiterForm_Load(object sender, EventArgs e)
         {
             this.Text = "Transmiter";
+
+            TabControl tbdynamic = new TabControl();
+            tbdynamic.Height = 200;
+            tbdynamic.Width = 200;
+
             // for each sensor in sensors
+            var reader = new ConfigReader();
+            var path = "e://sensorConfig.json";
+
+            // when
+            var result = reader.Read(path);
+            foreach ( var item in result.Sensors)
+            {
+                TabPage mPage = new TabPage();
+                mPage.Text = "ID: " + item.ID;
+                tbdynamic.TabPages.Add(mPage);
+            }
+           
+           
+            this.Controls.Add(tbdynamic);
+            tbdynamic.BringToFront();
             // create tab in tabs
             // for each tab in tabs create channel rabbitmq
         }
